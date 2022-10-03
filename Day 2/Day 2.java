79101a1ad9732c13f294b2a477ee71e3e39d8bf7
MAHIMA RAIKWAR <114341520+Mahimaraikwar28@users.noreply.github.com>
2022-10-04 00:17:04 +0530

    Update Day 2.java

--- Day 2/Day 2.java
+++ Day 2/Day 2.java
@@ -1,1 +1,23 @@
+import java.util.*;
+public class Day2 {
+    public static void main(String[] args) {
+        int rem=0,rev=0,temp=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num=sc.nextInt();
 
+        temp=num;
+        while(num>0) {
+            rem = num % 10;
+            rev = (rev * 10) + rem;
+            num = num / 10;
+        }
+            if(rev==temp)
+            {
+                System.out.println("IT IS AN PALINDROME NUMBER : )");
+            }
+            else{
+                System.out.println("IT IS NOT AN PALINDROME NUMBER : (");
+            }
+        }
+    }
